3f950b66e8c63be53af566dce42cc945663d8c0c
jdsteadycode <desaijeet11@gmail.com>
2025-07-29 19:36:22 +0530

    added Footer component for opinix-web

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,16 @@
+// grab utilities
+import "../App.css";
+
+// () -> Footer Component
+function Footer() {
+
+    // retun footer's jsx
+    return(
+        <footer className="app-footer">
+            @ULJ designs.ltd
+        </footer>
+    );
+}
+
+// expose to the project
+export {Footer};
