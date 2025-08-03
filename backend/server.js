8e77d904241b08960b22e78aabb2318528557721
jdsteadycode <desaijeet11@gmail.com>
2025-08-03 20:54:04 +0530

    added entrypoint for backend in opinix-web

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+const authRoutes = require('./routes/authRoutes');
+
+const app = express();
+
+app.use(cors());
+
+// ✅ Parse JSON first
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// serve static uploads
+// app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+
+// ✅ Mount routes AFTER body parsers
+app.use('/api/auth', authRoutes);
+
+// start the server
+app.listen((process.env.PORT || 5000), () => console.log(`Server running on port ${process.env.PORT || 5000}`));
