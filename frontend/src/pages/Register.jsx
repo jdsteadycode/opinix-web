1d818d17872752d523456907612285d0797787f2
jdsteadycode <desaijeet11@gmail.com>
2025-08-03 20:19:55 +0530

    added Registration for opinix-web

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,6 +1,6 @@
 // grab module(s)
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 
 // () -> Registeration Module
@@ -8,6 +8,19 @@ function Register() {
 
     // initial state array
     const [inputType, setInputType] = useState("text");
+
+    // initial navigation
+    const navigate = useNavigate();
+
+    // initial state-array for registration data
+    const [userData, setUserData] = useState({
+        "email": "",
+        "username": "",
+        "phone": "",
+        "password": "",
+        "confirmPassword": "",
+        "birthdate": "",
+    });
 
     // () -> handle the input type when focused!
     function handleInputFocusTextChange(event) {
@@ -18,19 +31,73 @@ function Register() {
         setInputType("date");
     }
 
+    // () -> track the form data changes
+    function handleFormData(event) {
+
+        // a new copy of fregistration data
+        let data = { ...userData };
+
+        // set the data
+        data[event.target.name] = event.target.value;
+
+        // update the state
+        setUserData(data);
+    }
+
+    // () -> handle user registration
+    async function handleRegistration(event) {
+
+        // restrict re-render
+        event.preventDefault();
+
+        // try to register the new user
+        try {
+            // response from the server
+            let response = await fetch(`http://localhost:5000/api/auth/register`, {
+                "method": "POST",
+                "headers": {"Content-Type": "application/json"},
+                "body": JSON.stringify(userData)
+            });
+
+            // parse the response from json
+            let data = await response.json();
+
+            // check log**
+            console.log(data);
+
+           // check if user is created
+            if(data.status) {
+
+                // navigate the user to `/login`
+                navigate("/login")
+            }
+           
+        }
+        // handle run-time errors
+        catch(error) {
+            // log the error
+            console.log(error);
+        }
+    }
+
+    // check log**
+    console.log(userData);
+
     // return jsx!
     return(
          <main className="app-auth-content">
-            <form className="auth-form">
+            <form className="auth-form" onSubmit={handleRegistration}>
                 <h1>Welcome folk!</h1>
-                <input type="text" placeholder="Your Email?" />
-                <input type="text" placeholder="Your username?" />
-                <input type="text" placeholder="Your phone-number?" />
-                <input type="password" placeholder="Your password?" />
-                <input type="password" placeholder="Confrim password?" /> 
+                <input type="text" name="email" placeholder="Your Email?" onChange={handleFormData} />
+                <input type="text" name="username" placeholder="Your username?" onChange={handleFormData} />
+                <input type="text" name="phone" placeholder="Your phone-number?" onChange={handleFormData} />
+                <input type="password" name="password" placeholder="Your password?" onChange={handleFormData} />
+                <input type="password" name="confirmPassword" placeholder="Confrim password?" onChange={handleFormData} /> 
                 <input 
                     type={inputType}
+                    name="birthdate"
                     placeholder="Your DOB?" 
+                    onChange={handleFormData}
                     onFocus={handleInputFocusTextChange} />
                 <p className="auth-register">
                     I have one!
