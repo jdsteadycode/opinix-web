d0b8a6799ab0ca0108e7d4f5a461c3bf5cdb877e
jdsteadycode <desaijeet11@gmail.com>
2025-08-26 16:28:14 +0530

    updated middleware and route set-up in opinix-web

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ const express = require('express');
 const cors = require('cors');
 const path = require('path');
 const authRoutes = require('./routes/authRoutes');
+const userRoutes = require('./routes/userRoutes');
+const pollRoutes = require('./routes/pollRoutes');
 
 const app = express();
 
@@ -13,10 +15,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // serve static uploads
-// app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 // ✅ Mount routes AFTER body parsers
 app.use('/api/auth', authRoutes);
+app.use('/api/user', userRoutes);
+app.use('/api/poll', pollRoutes);
 
 // start the server
 app.listen((process.env.PORT || 5000), () => console.log(`Server running on port ${process.env.PORT || 5000}`));
