98515606ea3e42fdafd527343ec6044cf0ce78d3
jdsteadycode <desaijeet11@gmail.com>
2025-09-10 12:50:13 +0530

    updated Poll.jsx

--- frontend/src/pages/Poll.jsx
+++ frontend/src/pages/Poll.jsx
@@ -3,7 +3,11 @@ import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 
 // () -> Poll Component (single poll)
-function Poll() {
+function Poll({ user_id }) {
+
+    // check log**
+    console.log(`user's id: ${user_id}`);
+
     // grab the id?
     const { id } = useParams();
 
@@ -18,6 +22,12 @@ function Poll() {
 
     // initial state array for selected-option(s)*
     const [selectedOptions, setSelectedOptions] = useState([]);
+
+    // initial state array for voting-status**
+    const [canVote, setCanVote] = useState(true);
+
+    // initial state array for voting.
+    const [isVoting, setIsVoting] = useState(false);
 
     // handle the API-call
     useEffect(function() {
@@ -35,7 +45,7 @@ function Poll() {
                 if(!pollsResponse.ok) {
 
                     // throw error
-                    throw new Error("Poll Fetch Request Failed!")
+                    throw new Error("Poll Fetch Request Failed!");
                 }
 
                 // get the data
@@ -46,6 +56,15 @@ function Poll() {
 
                 // update the state*
                 setPollDetails(prev => prev = pollDetails);
+
+                // verify for the further voting!
+                const response = await checkVoted(pollDetails.id);
+
+                // check log**
+                console.log(response);
+
+                // update the state
+                setCanVote(prev => prev = response.status);
             }
 
             // handle run-time errors
@@ -65,33 +84,131 @@ function Poll() {
                 setIsLoading(prev => prev = false);
             }
         }
-        // initial invoke!
-        fetchPoll();
-    }, []);
+
+        // () -> verifies user past votings (castings)**
+        async function checkVoted(poll_id) {
+
+            // safe**
+            try{
+
+                // try to make the request!
+                const pastResponse = await fetch(`http://localhost:5000/api/vote/check`, {
+                    "method": "POST",
+                    "headers": {
+                        "Content-Type": "application/json",
+                    },
+                    "body": JSON.stringify({
+                        "userId": user_id,
+                        "pollId": poll_id
+                    }),
+                });
+
+                // get the data
+                const data = await pastResponse.json();
+
+                // return the status
+                return data;
+            }
+
+            // handle run-time events
+            catch(error) {
+
+                // throw error
+                console.error(error);
+            }
+        }
+
+         // invoke the function(s)
+        fetchPoll();      
+
+    }, [id, user_id]);
+
 
     // handle selecting option
   const handleOptionChange = (optionId) => {
+
+    // for single
     if (pollDetails.poll_type === "single") {
-      setSelectedOptions([optionId]);
-    } else {
-      // toggle for multi-choice
+        
+        // update the state**
+        setSelectedOptions(prev => prev = [optionId]);
+    } 
+    // when multiple-polls `checkbox`
+    else {
+      // update the state for multi-choice**
       setSelectedOptions((prev) =>
         prev.includes(optionId)
           ? prev.filter((id) => id !== optionId)
           : [...prev, optionId]
       );
     }
+
+    // check log**
+    console.log(selectedOptions);
   };
 
   // dummy submit handler
   const handleVote = () => {
-    console.log("User voted on:", selectedOptions);
-    alert("Voting feature coming soon 🚀");
+
+    // check log**
+    console.log(`User of id ${user_id} casted for poll id ${pollDetails?.id} and for option(s): ${selectedOptions}`);
+    
+    // () -> handles vote on the poll!
+    async function castTheVote(uid, pid, opts) {
+
+        // safe**
+        try {
+            // update the state*
+            setIsVoting(true);
+
+            // handle the api-request!
+            const voteRequest = await fetch(`http://localhost:5000/api/vote/${pid}/cast`, {
+                "method": "POST",
+                "headers": {
+                    "Content-Type": "application/json",
+                },
+                "body": JSON.stringify({
+                    "selectedOptions": [...opts],
+                    "userId": uid
+                }),
+            });
+
+            // handle the incoming response
+            const voteResponse = await voteRequest.json();
+
+            // check log**
+            console.log(voteResponse);
+
+            // update the state
+            setCanVote(false);
+        }
+
+        // handle run-time errors
+        catch(error) {
+
+            // throw error
+            console.error(error);
+
+            // update the state
+            setError(error);
+        }
+
+        // at last
+        finally {
+
+            // update the state back!
+            setIsVoting(false);
+        }
+    }
+
+    // try to vote!
+    castTheVote(user_id, pollDetails?.id, selectedOptions);
   };
 
   // returns jsx.
   return (
     <main className="app-content">
+        {/* show error toasts */}
         {isLoading && <p>Loading polls...</p>}
         {error && <p style={{ color: "red" }}>{error}</p>}
 
@@ -130,10 +247,10 @@ function Poll() {
                     {pollDetails.options?.map((opt) => (
                     <div key={opt.id} className="poll-option-wrap">
                         <input
-                        type={pollDetails.poll_type === "single" ? "radio" : "checkbox"}
-                        name="poll-options"
-                        checked={selectedOptions.includes(opt.id)}
-                        onChange={() => handleOptionChange(opt.id)}
+                            type={pollDetails.poll_type === "single" ? "radio" : "checkbox"}
+                            name="poll-options"
+                            onChange={() => handleOptionChange(opt.id)}
+                            checked={selectedOptions.includes(opt.id)}
                         />
                         <label>{opt.text}</label>
 
@@ -161,13 +278,13 @@ function Poll() {
                     <button
                         className="create-btn"
                         type="button"
-                    // onClick={handleVote}
-                        disabled={selectedOptions.length === 0}
+                        onClick={handleVote}
+                        disabled={canVote === false || selectedOptions.length == 0 || isVoting}
                     >
-                    Submit Vote
+                    {isVoting ? "Submitting..." : "Submit"}
                     </button>
 
-                    {/* Expiration */}
+                    {/* Expiration */}  
                     {pollDetails.expires_at && (
                         <p className="expires">
                         Expires: {new Date(pollDetails.expires_at).toLocaleString()}
