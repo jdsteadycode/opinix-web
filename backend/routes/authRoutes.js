63180c24be26614efc616809e3fb793f6dac308f
jdsteadycode <desaijeet11@gmail.com>
2025-09-10 12:44:41 +0530

    updated routes for user and auth

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
-// const upload = require('../middleware/upload');
+const upload = require('../middleware/upload');
 
-// router.post('/register', upload.single('profileImage'), authController.register);
-router.post('/register', authController.register)
+router.post('/register', upload.single('profileImage'), authController.register);
+// router.post('/register', authController.register)
 router.post('/login', authController.login);
 
 module.exports = router;
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
+const upload = require('../middleware/upload');
 
 // set the methods
 router.get('/me', userController.me);
+// router.patch('/me/update', userController.updateMe);
+router.patch('/me/update', upload.single('profileImage'), userController.updateMe);
 
 // export the router
 module.exports = router;
