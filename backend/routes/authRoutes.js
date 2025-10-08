a5c2e965f8b8b630f308c8b720aeb7ec1cde8ff2
jdsteadycode <desaijeet11@gmail.com>
2025-10-08 14:32:18 +0530

    added new route for logout functionality for opinix-web

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -2,9 +2,11 @@ const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
 const upload = require('../middleware/upload');
+const verifyUser = require("../middleware/verifyUser");
 
 router.post('/register', upload.single('profileImage'), authController.register);
 // router.post('/register', authController.register)
 router.post('/login', authController.login);
+router.post('/logout', verifyUser, authController.logout);
 
 module.exports = router;
