40a51b778f7aa0b6bea836f0283d619cb98379e2
jdsteadycode <desaijeet11@gmail.com>
2025-10-08 14:33:10 +0530

    added new route for moderating poll for opinix-web

--- backend/routes/pollRoutes.js
+++ backend/routes/pollRoutes.js
@@ -3,12 +3,14 @@ const express = require('express');
 const router = express.Router();
 const pollController = require('../controllers/pollController.js');
 const upload = require("../middleware/upload.js");
+const pollModeration = require('../middleware/pollModeration');
 
 // set the methods
-router.post('/create',upload.any() , pollController.createPoll);
+router.post('/create',upload.any(), pollController.createPoll);
 router.get('/all', pollController.allPolls);
 router.get("/:id", pollController.singlePoll);
 router.post("/stats", pollController.fetchPollStats);
+router.post("/moderate", pollModeration);
 
 // export the router
 module.exports = router;
