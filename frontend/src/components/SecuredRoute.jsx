c640e320fbae1376d9a3b550892180c2b018a15a
jdsteadycode <desaijeet11@gmail.com>
2025-10-31 16:37:24 +0530

    updation auth for admin as well in opinix-web

--- frontend/src/components/SecuredRoute.jsx
+++ frontend/src/components/SecuredRoute.jsx
@@ -2,17 +2,23 @@
 import { Navigate } from "react-router-dom";
 
 // () -> SecuredRoute Component handle all authenticated-authorized Pages
-function SecuredRoute({ pages }) {
+function SecuredRoute({ pages, roleRequired = null }) {
 
     // check log**
-    console.log(pages);
+    // console.log(pages);
 
     // grab the token from storage
     let token = localStorage.getItem("token");
+    let role = localStorage.getItem("role");
 
     // when no `token` i.e., non-legit user then, redirect -> `/login`
-    // otherwise allow authenticated user!
-    return !token ? <Navigate to="/login" /> : pages;
+    if(!token) return <Navigate to="/login" />;
+
+    // authenticated, but not authorized for this route
+    if (roleRequired && role !== roleRequired)  return <Navigate to="/" />;
+    
+    // otherwise, its an normal user..
+    return pages;
 } 
 
 // expose to project
