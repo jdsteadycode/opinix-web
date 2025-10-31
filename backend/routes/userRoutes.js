c99c7e52a61496c0b839105006ac744e30db8c4b
jdsteadycode <desaijeet11@gmail.com>
2025-10-31 16:34:40 +0530

    updated routes for user in opinix-web

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -7,6 +7,9 @@ const upload = require('../middleware/upload');
 router.get('/me', userController.me);
 // router.patch('/me/update', userController.updateMe);
 router.patch('/me/update', upload.single('profileImage'), userController.updateMe);
+router.get("/me/:userId/my-polls", userController.getUserPolls);
+router.delete("/me/delete-poll/:pollId", userController.deletePoll);
+
 
 // export the router
 module.exports = router;
