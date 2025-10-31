7f03b53bd59ad24f2df5f329102e6186484424b1
jdsteadycode <desaijeet11@gmail.com>
2025-10-31 16:35:43 +0530

    added admin module for opinix-web

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ const authRoutes = require('./routes/authRoutes');
 const userRoutes = require('./routes/userRoutes');
 const pollRoutes = require('./routes/pollRoutes');
 const voteRoutes = require('./routes/voteRoutes');
+const adminRoutes = require('./routes/adminRoutes');
 
 const app = express();
 
@@ -23,6 +24,7 @@ app.use('/api/auth', authRoutes);
 app.use('/api/user', userRoutes);
 app.use('/api/poll', pollRoutes);
 app.use('/api/vote', voteRoutes);
+app.use("/api/admin", adminRoutes);
 
 // start the server
 app.listen((process.env.PORT || 5000), () => console.log(`Server running on port ${process.env.PORT || 5000}`));
