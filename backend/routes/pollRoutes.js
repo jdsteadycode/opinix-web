a9337e9d9230779b8959358f14f038084027f127
jdsteadycode <desaijeet11@gmail.com>
2025-10-31 16:34:16 +0530

    updated routes for poll in opinix-web

--- backend/routes/pollRoutes.js
+++ backend/routes/pollRoutes.js
@@ -11,6 +11,12 @@ router.get('/all', pollController.allPolls);
 router.get("/:id", pollController.singlePoll);
 router.post("/stats", pollController.fetchPollStats);
 router.post("/moderate", pollModeration);
+router.get("/voted/:userId", pollController.fetchVotedPolls);
+router.post("/report", pollController.reportPoll);
+router.delete("/admin/delete/:pollId", pollController.deleteReportedPoll);
+router.get("/admin/reported-polls", pollController.getReportedPolls);
+router.post("/admin/dismiss/:pollId", pollController.dismissReport);
+router.patch("/admin/restore/:pollId", pollController.restorePoll);
 
 // export the router
 module.exports = router;
