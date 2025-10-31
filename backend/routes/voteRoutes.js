bb28d9a0ecc8342801b13aa8a2337c80899553c7
jdsteadycode <desaijeet11@gmail.com>
2025-10-31 16:35:07 +0530

    updated routes for vote in opinix-web

--- backend/routes/voteRoutes.js
+++ backend/routes/voteRoutes.js
@@ -6,6 +6,7 @@ const voteController = require('../controllers/voteController.js');
 // set the method(s)
 router.post("/:pollId/cast", voteController.castVote);
 router.post("/check", voteController.voted);
+router.delete("/revoke-vote", voteController.revokeVote);
 
 // export the router
 module.exports = router;
